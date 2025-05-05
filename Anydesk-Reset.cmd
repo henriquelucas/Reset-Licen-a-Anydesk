bff49b6d02a6213345a6bdd82e6fd513e7e83ff6
Henrique Lucas de Sousa <31218658+henriquelucas@users.noreply.github.com>
2025-05-05 09:05:07 -0300

    Mudança realiza backups dos Favoritos
    Adicionando função pra realizar backup das linhas que guardam os favoritos, evitando assim a perca desses dados durante a redefinição das configurações.

--- Anydesk-Reset.cmd
+++ Anydesk-Reset.cmd
@@ -1,41 +1,36 @@
-@echo off & setlocal enableextensions
-title Resete AnyDesk
-reg query HKEY_USERS\S-1-5-19 >NUL || (echo Por favor, execute como administrador.& pause >NUL&exit)
-chcp 437
-call :stop_any
-del /f "%ALLUSERSPROFILE%\AnyDesk\service.conf"
-del /f "%APPDATA%\AnyDesk\service.conf"
-copy /y "%APPDATA%\AnyDesk\user.conf" "%temp%\"
-rd /s /q "%temp%\thumbnails" 2>NUL
-xcopy /c /e /h /r /y /i /k "%APPDATA%\AnyDesk\thumbnails" "%temp%\thumbnails"
-del /f /a /q "%ALLUSERSPROFILE%\AnyDesk\*"
+@echo off
+setlocal enabledelayedexpansion
+
+echo Fechando o AnyDesk...
+taskkill /f /im anydesk.exe >nul 2>&1
+
+echo Fazendo backup dos favoritos...
+set "anydesk_conf=%APPDATA%\AnyDesk\user.conf"
+set "favoritos_backup=%TEMP%\anydesk_favoritos.txt"
+
+if exist "%anydesk_conf%" (
+    for /f "delims=" %%a in ('findstr /b "ad.roster.favorites=" "%anydesk_conf%"') do (
+        echo %%a > "%favoritos_backup%"
+    )
+)
+
+echo Removendo configuracoes antigas...
 del /f /a /q "%APPDATA%\AnyDesk\*"
-call :start_any
-:lic
-type "%ALLUSERSPROFILE%\AnyDesk\system.conf" | find "ad.anynet.id=" || goto lic
-call :stop_any
-move /y "%temp%\user.conf" "%APPDATA%\AnyDesk\user.conf"
-xcopy /c /e /h /r /y /i /k "%temp%\thumbnails" "%APPDATA%\AnyDesk\thumbnails" 
-rd /s /q "%temp%\thumbnails"
-call :start_any
-echo *********
-echo Reset realizado com sucessso!.
-echo(
-goto :eof
- 
-:start_any
-sc start AnyDesk
-sc start AnyDesk
-if %errorlevel% neq 1056 goto start_any
-set AnyDesk1=%SystemDrive%\Program Files (x86)\AnyDesk\AnyDesk.exe
-set AnyDesk2=%SystemDrive%\Program Files\AnyDesk\AnyDesk.exe
-if exist "%AnyDesk1%" start "" "%AnyDesk1%"
-if exist "%AnyDesk2%" start "" "%AnyDesk2%"
-exit /b
- 
-:stop_any
-sc stop AnyDesk
-sc stop AnyDesk
-if %errorlevel% neq 1062 goto stop_any
-taskkill /f /im "AnyDesk.exe"
-exit /b
+rd /s /q "%APPDATA%\AnyDesk"
+
+echo Restaurando arquivos do backup...
+mkdir "%APPDATA%\AnyDesk"
+copy /y "C:\BackupAnyDesk\*.*" "%APPDATA%\AnyDesk\" >nul
+
+echo Restaurando favoritos...
+if exist "%favoritos_backup%" (
+    type "%favoritos_backup%" >> "%APPDATA%\AnyDesk\user.conf"
+    del /f "%favoritos_backup%"
+)
+
+echo Abrindo o AnyDesk...
+start "" "%ProgramFiles(x86)%\AnyDesk\AnyDesk.exe"
+
+echo Processo concluído.
+endlocal
+pause
