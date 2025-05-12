35962486259cb2fc328b306746aa0df936fb1010
Henrique Lucas de Sousa <31218658+henriquelucas@users.noreply.github.com>
2025-05-12 08:32:22 -0300

    Update Anydesk-Reset.cmd

--- Anydesk-Reset.cmd
+++ Anydesk-Reset.cmd
@@ -1,36 +1,70 @@
-@echo off
-setlocal enabledelayedexpansion
+@echo off & setlocal enableextensions
+title Resetar AnyDesk
 
-echo Fechando o AnyDesk...
-taskkill /f /im anydesk.exe >nul 2>&1
-
-echo Fazendo backup dos favoritos...
-set "anydesk_conf=%APPDATA%\AnyDesk\user.conf"
-set "favoritos_backup=%TEMP%\anydesk_favoritos.txt"
-
-if exist "%anydesk_conf%" (
-    for /f "delims=" %%a in ('findstr /b "ad.roster.favorites=" "%anydesk_conf%"') do (
-        echo %%a > "%favoritos_backup%"
-    )
+:: Verifica se o script está rodando como administrador (checa se a chave existe no registro)
+reg query HKEY_USERS\S-1-5-19 >NUL || (
+    echo Por favor, execute como administrador.
+    pause >NUL
+    exit
 )
 
-echo Removendo configuracoes antigas...
+chcp 437 :: Define o código de página
+
+call :stop_any
+
+:: Remove arquivos de configuração do AnyDesk
+del /f "%ALLUSERSPROFILE%\AnyDesk\service.conf"
+del /f "%APPDATA%\AnyDesk\service.conf"
+
+:: Salva o user.conf atual no TEMP
+copy /y "%APPDATA%\AnyDesk\user.conf" "%temp%\"
+
+:: Remove miniaturas antigas
+rd /s /q "%temp%\thumbnails" 2>NUL
+
+:: Copia as miniaturas atuais para o TEMP
+xcopy /c /e /h /r /y /i /k "%APPDATA%\AnyDesk\thumbnails" "%temp%\thumbnails"
+
+:: Remove todos os arquivos da pasta do AnyDesk (tanto do perfil do sistema quanto do usuário)
+del /f /a /q "%ALLUSERSPROFILE%\AnyDesk\*"
 del /f /a /q "%APPDATA%\AnyDesk\*"
-rd /s /q "%APPDATA%\AnyDesk"
 
-echo Restaurando arquivos do backup...
-mkdir "%APPDATA%\AnyDesk"
-copy /y "C:\BackupAnyDesk\*.*" "%APPDATA%\AnyDesk\" >nul
+call :start_any
 
-echo Restaurando favoritos...
-if exist "%favoritos_backup%" (
-    type "%favoritos_backup%" >> "%APPDATA%\AnyDesk\user.conf"
-    del /f "%favoritos_backup%"
-)
+:lic
+:: Aguarda até o arquivo system.conf conter a linha "ad.anynet.id="
+type "%ALLUSERSPROFILE%\AnyDesk\system.conf" | find "ad.anynet.id=" || goto lic
 
-echo Abrindo o AnyDesk...
-start "" "%ProgramFiles(x86)%\AnyDesk\AnyDesk.exe"
+:: Restaura os arquivos de configuração
+call :stop_any
+move /y "%temp%\user.conf" "%APPDATA%\AnyDesk\user.conf"
+xcopy /c /e /h /r /y /i /k "%temp%\thumbnails" "%APPDATA%\AnyDesk\thumbnails"
+rd /s /q "%temp%\thumbnails"
 
-echo Processo concluído.
-endlocal
-pause
+call :start_any
+
+echo *********
+echo Concluído.
+echo(
+goto :eof
+
+:start_any
+:: Inicia o serviço AnyDesk
+sc start AnyDesk
+sc start AnyDesk
+if %errorlevel% neq 1056 goto start_any
+
+:: Tenta iniciar o executável, se existir
+set AnyDesk1=%SystemDrive%\Program Files (x86)\AnyDesk\AnyDesk.exe
+set AnyDesk2=%SystemDrive%\Program Files\AnyDesk\AnyDesk.exe
+if exist "%AnyDesk1%" start "" "%AnyDesk1%"
+if exist "%AnyDesk2%" start "" "%AnyDesk2%"
+exit /b
+
+:stop_any
+:: Para o serviço e mata o processo
+sc stop AnyDesk
+sc stop AnyDesk
+if %errorlevel% neq 1062 goto stop_any
+taskkill /f /im "AnyDesk.exe"
+exit /b
